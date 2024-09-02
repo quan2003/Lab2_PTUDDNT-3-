e1908b4ee8815806fa10ef48fe0865fefbff5728
quan2003 <luuquan232003@gmail.com>
2024-09-02 23:03:22 +0700

    lab2

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,83 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-    home: Scaffold(
-    backgroundColor: Colors.teal,
-    body: SafeArea(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-       children: <Widget>[
-        Container(
-  width: 30.0,
-  color: Colors.white,
-   child: Text('Container 1'),
-),
-SizedBox(
-  width: 30.0,
-),
-Container(
-color: Colors.blue ,
-child: Text('Container 2')
-),
-Container(
-color: Colors.red,
-child: Text('Container 3')
-)
-       ],
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/image.jpg'),
+            ),
+            Text(
+              'Lưu Quân',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  fontFamily: 'Source Code Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+84 336 440 523',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Code Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'quantl.21it@vku.udn.vn',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Code Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )),
+          ],
+        )),
       ),
-    ),
-    ),
     );
-    }
-
-
+  }
 }
