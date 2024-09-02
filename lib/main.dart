f764e14e60229b39e2e1a0797e2ca2d98242c7f7
NguyenThiNguyen <nguyennt2.22it@vku.udn.vn>
2024-09-02 13:06:17 +0700

    w2

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Scaffold(
+    backgroundColor: Colors.teal,
+    body: SafeArea(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+       children: <Widget>[
+        Container(
+  width: 30.0,
+  color: Colors.white,
+   child: Text('Container 1'),
+),
+SizedBox(
+  width: 30.0,
+),
+Container(
+color: Colors.blue ,
+child: Text('Container 2')
+),
+Container(
+color: Colors.red,
+child: Text('Container 3')
+)
+       ],
+      ),
+    ),
+    ),
+    );
+    }
+
+
+}
